9c55775c7c7df814c72ba6527f1229870e146c8c
Jhan Carlos Carvajal Bastidas <liceo@DESKTOP-LNJO0VF>
2021-04-16 18:23:57 -0500

    I fixed the errors that the program had and added comments to make it
    clear how it works

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -8,56 +8,72 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
-
 public class Main {
-	
-	//Towers
+
+	// Towers
 	private static int t1 = 1;
 	private static int t2 = 2;
 	private static int t3 = 3;
-	
-	//Number of discs in every tower
+
+	// Number of discs in every tower
 	private static int discs_t1;
 	private static int discs_t2;
 	private static int discs_t3;
-	
+
 	private static PrintWriter pWriter;
 	private static BufferedReader bReader;
-	
-	
+
 	public static void main(String[] args) throws IOException {
-		File file = new File("output.txt");
-		FileReader fileReader = new FileReader("input.txt");
+
+		File file = new File("output.txt"); // make a file where will be print the outputs of Hanoi Towers
+		FileReader fileReader = new FileReader("input.txt"); // search a file called "input.txt" where is the amount of
+																// cases and the discs for every case
 		pWriter = new PrintWriter(file);
 		bReader = new BufferedReader(fileReader);
+
+		// variables
+
 		String line;
 		int discs;
-		ArrayList<String> input = new ArrayList<String>();
-		
+		ArrayList<String> input = new ArrayList<String>(); // in this ArrayList the program save the lines from file.txt
+
+		// read input.txt
 		try {
-			
+
 			while ((line = bReader.readLine()) != null) {
 				input.add(line);
 			}
 			bReader.close();
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
-		for (int i = 1; i <= Integer.parseInt(input.get(0)); i++) {
-			discs_t1 = Integer.parseInt(input.get(i));
-			discs = Integer.parseInt(input.get(i));
-			pWriter.println(discs_t1 + " " + discs_t2 + " " + discs_t3);
+
+		int amount_cases = Integer.parseInt(input.get(0));
+		input.remove(0);// it is removed from the ArrayList to work only with the amount of discs for every case
+
+		for (int i = 0; i < amount_cases; i++) {
+			
+			discs_t1 = Integer.parseInt(input.get(i)); // At first line the discs are in the tower one that's the reason for this assignment
+			
+			discs = Integer.parseInt(input.get(i)); // here the program gets the amount of discs for the current case
+			
+			pWriter.println(discs_t1 + " " + discs_t2 + " " + discs_t3); // this line print at the output.txt the first case where the discs are in the tower one
+			
 			hanoi(discs, t1, t3, t2);
 			
-			pWriter.close();
+			pWriter.println(" ");
 			
+			//Now, the variables are reestablished and they are ready for the next case
+			discs = 0;
+			discs_t1 = 0;
+			discs_t2 = 0;
+			discs_t3 = 0;
 		}
-		
-			
+		pWriter.close();
+
 	}
-	
+
 	public static void hanoi(int discs, int t1, int t3, int t2) {
 
 		if (discs == 1) {
@@ -65,12 +81,14 @@ public class Main {
 			return;
 		}
 		hanoi(discs - 1, t1, t2, t3);
+		
 		showAnswers(t1, t3);
+		
 		hanoi(discs - 1, t2, t3, t1);
 	}
-	
-	
+
 	public static void showAnswers(int t1, int t3) {
+		
 		if (t1 == 1) {
 			discs_t1--;
 		}
@@ -80,7 +98,7 @@ public class Main {
 		if (t1 == 3) {
 			discs_t3--;
 		}
-		
+
 		if (t3 == 1) {
 			discs_t1++;
 		}
@@ -92,5 +110,5 @@ public class Main {
 		}
 		pWriter.println(discs_t1 + " " + discs_t2 + " " + discs_t3);
 	}
-	
+
 }
